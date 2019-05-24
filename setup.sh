07587bb9a200f1768d9f2587e1f0226ab3ed1090
ytebine07 <ytebine07@gmail.com>
2019-05-24 23:38:47 +0900

    [setup.sh]gitの設定をスムーズに

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #/bin/bash
 
 
-##############################################
-# 1.各種dotfileから自前の設定を読む
-##############################################
+echo "##############################################"
 echo "1.各種dotfileから自前の設定を読む"
+echo "##############################################"
 DOT_FILES=(
             .vimrc
             .bashrc
@@ -25,18 +24,16 @@ do
     fi
 done
 
-##############################################
-# 2.neobundle.vimを入れる
-##############################################
+echo "##############################################"
 echo "2.neobundle.vimを入れる"
+echo "##############################################"
 if [ ! -d ~/dotfiles/vim/plugins/neobundle.vim ]; then
     git clone git://github.com/Shougo/neobundle.vim ~/dotfiles/vim/plugins/neobundle.vim
 fi
 
-##############################################
-# 3.git-completion.bashを入れる
-##############################################
+echo "##############################################"
 echo "3.git-completion.bashを入れる"
+echo "##############################################"
 if [ ! -s git-completion.bash ]; then
     echo "nai"
     curl -s -f -L https://raw.github.com/git/git/master/contrib/completion/git-completion.bash > git-completion.bash
@@ -47,11 +44,25 @@ fi
 if [ ! -s git-prompt.sh ]; then
 curl -s -f -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > git-prompt.sh
 fi
-##############################################
-# 4.gitのショートカット
-##############################################
-echo "4.gitのショートカット"
+
+echo "##############################################"
+echo "4.gitの設定"
+echo "##############################################"
+git config --global user.name > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    read -p "Enter your git user.name [ytebine07]: " GITUSERNAME
+    GITUSERNAME=${GITUSERNAME:-ytebine07}
+    git config --global user.name "$GITUSERNAME"
+fi
+git config --global user.email > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    read -p "Enter your git user.email [ytebine07@gmail.com]: " GITUSEREMAIL
+    GITUSEREMAIL=${GITUSEREMAIL:-ytebine07@gmail.com}
+    git config --global user.email "$GITUSEREMAIL"
+fi
 git config --global alias.br "branch"
 git config --global alias.co "checkout"
 git config --global alias.st "status"
 git config --global color.ui auto
+echo "[config list]"
+git config --global --list
